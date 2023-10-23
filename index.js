51e988e62be95f15635648a882874cf0cf8df20b
amir811537 <amirhossain.bc.75@gmail.com>
2023-10-23 13:55:14 +0600

    data update fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
@@ -8,7 +8,12 @@ const app = express();
 // sHiKJoo3fAljOTPP
 
 // midewaare
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
@@ -28,13 +33,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db("productDB").collection("products");
 
     //ADDING PRODUCTS
     app.post("/products", async (req, res) => {
       const product = req.body;
+      // console.log('get product',product)
       const result = await productCollection.insertOne(product);
       res.send(result);
     });
@@ -45,6 +51,53 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+     // Get products by brand
+    app.get("/products/:brand", async (req, res) => {
+      const brandName = req.params.brand;
+   
+      const query = { brandname: brandName.toLowerCase() };
+      // console.log(query)
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get singel product by id
+    app.get('/productsbyid/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result =await productCollection.findOne(query);
+      res.send(result)
+    })
+
+// updated a products
+app.put('/products/:id',async(req,res)=>{
+  const id =req.params.id;
+  const filter ={_id: new ObjectId(id)};
+  const options ={upsert:true};
+  const updatedProduct=req.body;
+  const product ={ 
+    $set : {
+      photourl:updatedProduct.photourl, 
+      brandname:updatedProduct.brandname,
+       name:updatedProduct.name,
+       price:updatedProduct.price, 
+       rating:updatedProduct.rating,
+        type: updatedProduct.type
+    }
+  }
+  const result =await productCollection.updateOne(filter,product,options)
+  res.send(result)
+})
+
+// delete a data 
+app.delete('/products/:id',async(req,res)=>{
+  const id =req.params.id;
+  const query ={_id: new ObjectId(id)}
+  const result =await productCollection.deleteOne(query);
+  res.send(result)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
