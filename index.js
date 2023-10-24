62ca34ba2c69cb184ef9b74c49a288eefcc62fee
amir811537 <amirhossain.bc.75@gmail.com>
2023-10-24 23:04:07 +0600

    user wish data added to my cart

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -8,19 +9,18 @@ const app = express();
 // sHiKJoo3fAljOTPP
 
 // midewaare
-const corsOptions ={
-  origin:'*', 
-  credentials:true,
-  optionSuccessStatus:200,
-}
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-const uri =
-  "mongodb+srv://amirhossainbc75:sHiKJoo3fAljOTPP@cluster0.3yb9d5d.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3yb9d5d.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -37,6 +37,9 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("products");
 
+    // user collection
+    const usercollection = client.db("productDB").collection("user");
+
     //ADDING PRODUCTS
     app.post("/products", async (req, res) => {
       const product = req.body;
@@ -52,10 +55,10 @@ async function run() {
       res.send(result);
     });
 
-     // Get products by brand
+    // Get products by brand
     app.get("/products/:brand", async (req, res) => {
       const brandName = req.params.brand;
-   
+
       const query = { brandname: brandName.toLowerCase() };
       // console.log(query)
       const cursor = productCollection.find(query);
@@ -64,40 +67,58 @@ async function run() {
     });
 
     // get singel product by id
-    app.get('/productsbyid/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query={_id: new ObjectId(id)}
-      const result =await productCollection.findOne(query);
-      res.send(result)
-    })
+    app.get("/productsbyid/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
-// updated a products
-app.put('/products/:id',async(req,res)=>{
-  const id =req.params.id;
-  const filter ={_id: new ObjectId(id)};
-  const options ={upsert:true};
-  const updatedProduct=req.body;
-  const product ={ 
-    $set : {
-      photourl:updatedProduct.photourl, 
-      brandname:updatedProduct.brandname,
-       name:updatedProduct.name,
-       price:updatedProduct.price, 
-       rating:updatedProduct.rating,
-        type: updatedProduct.type
-    }
-  }
-  const result =await productCollection.updateOne(filter,product,options)
-  res.send(result)
-})
+    // updated a products
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+          photourl: updatedProduct.photourl,
+          brandname: updatedProduct.brandname,
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          type: updatedProduct.type,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
 
-// delete a data 
-app.delete('/products/:id',async(req,res)=>{
-  const id =req.params.id;
-  const query ={_id: new ObjectId(id)}
-  const result =await productCollection.deleteOne(query);
-  res.send(result)
-})
+    // delete a data
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    //user related api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usercollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/userProduct/:email", async (req, res) => {
+      const email=req.params.email;
+      const query = { email: email };
+      const cursor = usercollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
